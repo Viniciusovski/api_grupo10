2b0375d774e9b5f35fb9c4dd74fee6703bb4b040
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-10-22 15:47:51 -0300

    Aluno model

---
+++ desafiogenerations/src/main/java/desafiogenerations/models/Turma.java
@@ -0,0 +1,54 @@
+package desafiogenerations.models;
+
+import jakarta.persistence.*;
+
+
+import java.util.List;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"nome"})})
+public class Turma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String instrutor;
+
+    @OneToMany(mappedBy = "turma", cascade = CascadeType.ALL)  // 'turma' refere-se ao campo em 'Aluno'
+    private List<Aluno> alunos;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public void setInstrutor(String instrutor) {
+        this.instrutor = instrutor;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+}
